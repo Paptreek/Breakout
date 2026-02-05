83a9f08e159fa42a023da20113249e450f0c28b1
paptreek <paptreek@hotmail.com>
2026-02-05 15:38:52 -0600

    paddle movement
    - player paddle now moves with mouse position
    - player paddle stays within screen bounds

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,15 +3,29 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    private Vector2 _mousePosition;
-
-    void Start()
-    {
-        _mousePosition = Mouse.current.position.value;
-    }
+    public GameObject leftWall;
+    public GameObject rightWall;
+    private Vector3 _mousePosition;
 
     void Update()
     {
-        Debug.Log(_mousePosition);
+        _mousePosition = Camera.main.ScreenToWorldPoint(Mouse.current.position.value);
+        _mousePosition.y = transform.position.y;
+        _mousePosition.z = transform.position.z;
+
+        MovePaddleWithinScreen();
+    }
+
+    private void MovePaddleWithinScreen()
+    {
+        float cursor = _mousePosition.x;
+        float halfOfPaddle = GetComponent<Renderer>().bounds.size.x;
+        float rightEdgeOfLeftWall = leftWall.GetComponent<Renderer>().bounds.max.x;
+        float leftEdgeOfRightWall = rightWall.GetComponent<Renderer>().bounds.min.x;
+
+        if (cursor - halfOfPaddle / 2 >= rightEdgeOfLeftWall && cursor + halfOfPaddle / 2 <= leftEdgeOfRightWall)
+        {
+            transform.position = _mousePosition;
+        }
     }
 }
