dd2efbd70d4bad818b62c8f47f3f36ff769b4075
paptreek <paptreek@hotmail.com>
2026-02-06 18:52:35 -0600

    speed increase and sound
    - ball speed now increases only when breaking a brick
    - sound effects play when ball hits brick, paddle, or wall

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -3,6 +3,12 @@ using UnityEngine;
 public class Ball : MonoBehaviour
 {
     public GameObject player;
+    public GameObject brick;
+    public GameObject wall;
+
+    public GameObject brickSound;
+    public GameObject paddleSound;
+    public GameObject wallSound;
 
     private Rigidbody2D _rb;
     private float moveSpeed = 250.0f;
@@ -25,16 +31,15 @@ public class Ball : MonoBehaviour
     private void OnCollisionEnter2D(Collision2D collision)
     {
         Vector2 normal = collision.GetContact(0).normal;
-        _rb.AddForce(normal * 5);
 
         float contactPoint = collision.GetContact(0).point.x;
-
-        float centerOfPaddle = player.GetComponent<BoxCollider2D>().bounds.center.x;
         float leftOfPaddle = player.GetComponent<BoxCollider2D>().bounds.min.x;
         float rightOfPaddle = player.GetComponent<BoxCollider2D>().bounds.max.x;
 
         if (collision.gameObject == player)
         {
+            paddleSound.gameObject.GetComponent<AudioSource>().Play();
+
             if (contactPoint == leftOfPaddle)
             {
                 // if ball collides with left edge of the paddle, direction changes to negative
@@ -47,10 +52,18 @@ public class Ball : MonoBehaviour
                 _rb.linearVelocity = new Vector2(Mathf.Abs(_rb.linearVelocity.x), _rb.linearVelocity.y);
                 Debug.Log($"Collided with RIGHT edge! New direction: {_rb.linearVelocity.x}");
             }
-            else
-            {
-                Debug.Log($"Collided with CENTER!");
-            }
+        }
+
+        if (collision.gameObject.CompareTag("Brick"))
+        {
+            _rb.AddForce(normal * 5);
+
+            brickSound.gameObject.GetComponent<AudioSource>().Play();
+        }
+
+        if (collision.gameObject.CompareTag("Wall"))
+        {
+            wallSound.gameObject.GetComponent<AudioSource>().Play();
         }
     }
 }
