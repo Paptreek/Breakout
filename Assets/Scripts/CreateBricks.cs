d528a414e5e156ef653dfb3a5371848ff0190b06
paptreek <paptreek@hotmail.com>
2026-02-07 22:37:16 -0600

    brick colors
    - added colors to the bricks

--- Assets/Scripts/CreateBricks.cs
+++ Assets/Scripts/CreateBricks.cs
@@ -7,6 +7,7 @@ public class CreateBricks : MonoBehaviour
     void Awake()
     {
         GameObject[] bricks = new GameObject[10];
+        int counter = 0;
 
         int rows = 3;
         int columns = 10;
@@ -14,16 +15,29 @@ public class CreateBricks : MonoBehaviour
         float x = -6.5f;
         float y = 4.0f;
 
+        SetColor(Color.softRed);
+
         for (int row = 0; row < rows; row++)
         {
             for (int col = 0; col < columns; col++)
             {
                 Instantiate(brick, new Vector3(x, y, 0), new Quaternion().normalized);
                 x += 1.46f;
+
+                counter++;
+
+                if (counter < 10) SetColor(Color.softRed);
+                if (counter >= 10) SetColor(Color.softBlue);
+                if (counter >= 20) SetColor(Color.softGreen);
             }
 
             x = -6.5f;
             y -= 0.75f;
         }
+    }
+
+    private void SetColor(Color color)
+    {
+        brick.GetComponent<SpriteRenderer>().color = color;
     }
 }
